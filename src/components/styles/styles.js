f5183a7d75432e8de9f5cea45ff8398e5030fd3d
Mckenna Poulos <poulos.m@husky.neu.edu>
2020-11-08 21:42:51 -0500

    add global color and font styles

---
+++ src/components/styles/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+// Colors
+export const PRIMARY_YELLOW = "#FFD43D";
+export const PRIMARY_GREEN = "#74D395";
+export const PRIMARY_NAVY = "#023047";
+export const PRIMARY_RED = "#FF6752";
+export const PRIMARY_ORANGE = "#FF9D42";
+
+export const TEXT_BLACK = "#1A1A1A";
+export const BG_WHITE = "#F8F8F8";
+
+// Fonts
+export const H1 = styled.h1`
+font-family: 'Work Sans', sans-serif;
+  font-size: 55px;
+`
+
+export const H2 = styled.h2`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 22px;
+  font-style: italic;
+  font-weight: 300;
+`
+
+export const P1 = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 16px;
+  font-weight: normal;
+`
+
+export const P2 = styled.p`
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+`
