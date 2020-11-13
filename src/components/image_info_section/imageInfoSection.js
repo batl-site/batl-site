a05ede07939133fb51629fc414ad1f5b61b10685
MacKenzie Kerwin <kerwin.m@husky.neu.edu>
2020-11-13 16:54:56 -0500

    wrapped component in responsive container

--- src/components/image_info_section/imageInfoSection.js
+++ src/components/image_info_section/imageInfoSection.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { H1, H2, P1 } from '../styles/styles'
 import Images from './images/images'
+import { Container } from './styled'
 
 const ImageInfoSection = ({ section, content, variant }) => (
-  <div className="row align-items-center">
+  <Container className="d-flex flex-wrap align-items-center">
     <div className="col-l2 col-lg-5 d-flex justify-content-center">
       <Images images={content.images} variant={variant} />
     </div>
@@ -12,7 +13,7 @@ const ImageInfoSection = ({ section, content, variant }) => (
       <H1>{content.heading}</H1>
       <P1>{content.description}</P1>
     </div>
-  </div> 
+  </Container>
 )
 
 export default ImageInfoSection
---
+++ src/components/image_info_section/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  @media (min-width: 992px) {
+    max-width: 960px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+`
