32d19a15479c16bf28bef515114ba78bead784cd
Mckenna Poulos <poulos.m@husky.neu.edu>
2020-11-13 17:00:16 -0500

    yellow color block implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import Layout from "../components/layout";
 import MissionStatement from "../components/mission/missionStatement";
 import SEO from "../components/seo";
+import { H1, H2, P1, P2 } from "../components/styles/styles";
+import YellowColorBlock from "../components/yellowColorBlock/yellowColorBlock";
 import { HOMEPAGE_ID } from "../constants/constants";
 import apiService from "../service/apiService";
 
@@ -24,12 +26,16 @@ const HomePage = () => {
     description: pageContent.missionStatementDescription,
     imageSrc: pageContent.missionStatementImage.fields.file
   } : null
+  const heroContent = pageContent.homepageHero ? pageContent.homepageHero.fields : null
+  const YCBContent = pageContent.more ? pageContent.more.fields : null
+
+  console.log(pageContent)
 
   return (
     <Layout>
       <SEO title="Home" />
       {missionContent && (<MissionStatement content={missionContent} />)}
-
+      {YCBContent && <YellowColorBlock content={YCBContent}/>}
 
 
     </Layout>
