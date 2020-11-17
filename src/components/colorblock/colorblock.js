58ecafc58d52ffbba57ce40c54fc879cd647ca90
Mckenna Poulos <poulos.m@husky.neu.edu>
2020-11-17 12:24:55 -0500

    apply changes

--- src/components/colorblock/colorblock.js
+++ src/components/colorblock/colorblock.js
@@ -16,12 +16,12 @@ const ColorBlock = ({ content, color }) => {
   return (
     <ColorSection color={color}>
       <RelativeContainer>
-        <Header className="col-10 col-md-8 col-lg-6 col-xl-4">
+        <Header>
           <H1>{content.heading}</H1>
         </Header>
       </RelativeContainer>
       <Content>
-        <OneColumn className="d-block d-sm-none d-col-12">
+        <OneColumn>
           <P1>{content.description}</P1>
           <Button
             className="mt-4"
@@ -32,7 +32,7 @@ const ColorBlock = ({ content, color }) => {
             </ButtonText>
           </Button>
         </OneColumn>
-        <TwoColumn className="d-none d-sm-block col-11 col-md-10 col-lg-8 col-xl-8 ">
+        <TwoColumn>
           <P1>{content.description}</P1>
           <Button
             className="mt-4"
