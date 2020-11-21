c7c85733e12501d80aa1f2883b20763474d38da6
Mckenna Poulos <poulos.m@husky.neu.edu>
2020-11-20 22:07:16 -0800

    clean up code

--- src/components/mission/styled.js
+++ src/components/mission/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const ImageWrapper = styled.div.attrs({className: "mb-5"})`
+export const ImageWrapper = styled.div.attrs({ className: "mb-5" })`
   float: right;
   padding: 0 !important;
   width: 80vw;
@@ -14,7 +14,9 @@ export const ImageWrapper = styled.div.attrs({className: "mb-5"})`
   }
 `;
 
-export const Description = styled.div.attrs({className:"col-12 col-md-6 col-lg-4"})`
+export const Description = styled.div.attrs({
+  className: "col-12 col-md-6 col-lg-4",
+})`
   padding: 0 !important;
 
   @media (min-width: 768px) {
@@ -22,9 +24,8 @@ export const Description = styled.div.attrs({className:"col-12 col-md-6 col-lg-4
   }
 
   @media (min-width: 1550px) {
-      padding-left: 0 !important;
+    padding-left: 0 !important;
   }
-
 `;
 
 export const BleedBreakPoint = styled.div`
@@ -38,46 +39,15 @@ export const BleedBreakPoint = styled.div`
   }
 `;
 
-// @media (min-width: 1200px){
-//     max-width: 1140px;
-//     width: 100%;
-//     padding-right: 15px;
-//     padding-left: 15px;
-//     margin-right: auto;
-//     margin-left: auto;
-//   }
-//   @media (min-width: 992px) {
-//     max-width: 960px;
-//     width: 100%;
-//     padding-right: 15px;
-//     padding-left: 15px;
-//     margin-right: auto;
-//     margin-left: auto;
-//   }
-//   @media (min-width: 768px) {
-//     max-width: 720px;
-//     width: 100%;
-//     padding-right: 15px;
-//     padding-left: 15px;
-//     margin-right: auto;
-//     margin-left: auto;
-//   }
-
-//   @media (min-width: 576px) {
-//     max-width: 540px;
-//     width: 100%;
-//     padding-right: 15px;
-//     padding-left: 15px;
-//     margin-right: auto;
-//     margin-left: auto;
-//   }
-
-export const Content = styled.div.attrs({className: "d-none d-md-inline-flex"})`
+export const Content = styled.div.attrs({
+  className: "d-none d-md-inline-flex",
+})`
   justify-content: space-between;
 
   @media (min-width: 1550px) {
     max-width: 1140px;
     width: 100%;
     margin-right: auto !important;
-    margin-left: auto  !important;
-  }`;
+    margin-left: auto !important;
+  }
+`;
