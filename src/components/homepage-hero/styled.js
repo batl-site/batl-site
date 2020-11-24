fa44ff8ba13e4dbf91cfb70043c542c0dfb536bc
Mckenna Poulos <poulos.m@husky.neu.edu>
2020-11-23 18:30:37 -0800

    apply pr comments

--- src/components/homepage-hero/styled.js
+++ src/components/homepage-hero/styled.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 import { PRIMARY_YELLOW } from "../styles/styles";
+import { OVERLAY_ZINDEX } from "../../constants/constants";
 
-export const HpHeroContainer = styled.div.attrs({classname: "d-flex flex-column"})`
-    margin-bottom: 25vh;
-`
+export const HpHeroContainer = styled.div.attrs({
+  classname: "d-flex flex-column",
+})`
+  margin-bottom: 25vh;
+`;
 
 export const HeroImage = styled.div`
   background-image: ${(props) => (props.image ? `url(${props.image})` : null)};
@@ -13,32 +16,35 @@ export const HeroImage = styled.div`
   background-position: 50% 50%;
 `;
 
-export const HeroImageContainer = styled.div.attrs({className: "container-fluid"})`
-    height: 70vh;
-    padding: 0 !important;
+export const HeroImageContainer = styled.div.attrs({
+  className: "container-fluid p-0",
+})`
+  height: 70vh;
 
-    @media (min-width: 1550px) {
-        height: 60vh;
-    }
-`
+  @media (min-width: 1550px) {
+    height: 60vh;
+  }
+`;
 
-export const HeroBlockOverlay = styled.div.attrs({className:"col-11 col-md-7"})`
-    background-color: ${PRIMARY_YELLOW};
-    position: relative !important;
+export const HeroBlockOverlay = styled.div.attrs({
+  className: "col-11 col-md-7 position-relative",
+})`
+  background-color: ${PRIMARY_YELLOW};
 
-    z-index: 10000;
-    padding: 60px !important;
-    overflow: hidden;
-    bottom: 40vh;
+  z-index: ${OVERLAY_ZINDEX};
+  padding: 60px !important;
+  overflow: hidden;
+  bottom: 40vh;
 
-    @media (min-width: 1550px) {
-        top: -35vh;
-    }
+  @media (min-width: 1550px) {
+    top: -35vh;
+  }
+`;
 
-`
+export const HeroBlockPosition = styled.div.attrs({
+  className: "position-absolute",
+})`
+  margin-left: -60px;
+`;
 
-export const HeroBlockPosition = styled.div.attrs({className: "position-absolute"})`
-    margin-left: -60px;
-`
-
-export const Container = styled.div.attrs({className: "container"})``
+export const Container = styled.div.attrs({ className: "container" })``;
