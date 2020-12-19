b66cd2f7d9c6febd2614bee8c826a14616c80e57
MacKenzie Kerwin <kerwin.m@husky.neu.edu>
2020-12-18 20:49:00 -0500

    timeline moment sprinkles

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -2,20 +2,43 @@ import React from "react";
 import TimelineMoment from "./timeline_moment/timelineMoment";
 import { Container, Line, ImageContainer, Sprinkle } from "./styled";
 import { MediumImage } from "../image_info_section/images/styled";
-import BigYellow from "../../images/sprinkles/filled-small-yellow.svg";
+import FilledYellow from "../../images/sprinkles/filled-yellow.svg";
+import FilledNavy from "../../images/sprinkles/filled-navy.svg";
+import OutlinedNavy from "../../images/sprinkles/outlined-navy.svg";
+import OutlinedGreen from "../../images/sprinkles/outlined-green.svg";
+import YellowWedge from "../../images/sprinkles/left-wedge-yellow.svg";
+import NavyWedge from "../../images/sprinkles/left-wedge-navy.svg";
+import GreenWedge from "../../images/sprinkles/left-wedge-green.svg";
 
-const Timeline = ({ moments, image }) => (
-  <Container>
-    <Line>
-      {moments.map((moment, i) => (
-        <TimelineMoment key={i} indent={(i + 1) % 3} content={moment.fields} />
-      ))}
-    </Line>
-    <ImageContainer>
-      <Sprinkle src={BigYellow} />
-      <MediumImage src={image} />
-    </ImageContainer>
-  </Container>
-);
+const Timeline = ({ moments, image }) => {
+  const sprinkles = [
+    OutlinedNavy,
+    GreenWedge,
+    YellowWedge,
+    FilledNavy,
+    YellowWedge,
+    GreenWedge,
+    OutlinedGreen,
+    NavyWedge,
+  ];
+  return (
+    <Container>
+      <Line>
+        {moments.map((moment, i) => (
+          <TimelineMoment
+            key={i}
+            indent={i % 3}
+            content={moment.fields}
+            sprinkle={sprinkles[i % 8]}
+          />
+        ))}
+      </Line>
+      <ImageContainer>
+        <Sprinkle src={FilledYellow} />
+        <MediumImage src={image} />
+      </ImageContainer>
+    </Container>
+  );
+};
 
 export default Timeline;
--- src/components/timeline/timeline_moment/styled.js
+++ src/components/timeline/timeline_moment/styled.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import { TEXT_BLACK, H3, RoundImage } from "../../styles/styles";
-import { MIN_SM_DESKTOP_SIZE } from "../../../constants/constants";
+import {
+  MIN_SM_DESKTOP_SIZE,
+  OVERLAY_ZINDEX,
+} from "../../../constants/constants";
 
 export const ContentWrapper = styled.div`
   display: flex;
@@ -13,7 +16,7 @@ export const ContentWrapper = styled.div`
 `;
 
 export const Indent = styled.div.attrs((props) => ({
-  className: `col-1 ${props.size === 1 ? "col-md-3" : "col-md-6"}`,
+  className: `col-1 ${props.size === 0 ? "col-md-3" : "col-md-6"}`,
 }))`
   border-top: 1px dashed ${TEXT_BLACK};
   margin-top: ${(props) => (props.dateExists ? "48px" : "9px")};
@@ -39,16 +42,55 @@ export const Title = styled(H3)`
   margin-bottom: 1.5em;
 `;
 
-export const Image = styled(RoundImage).attrs({
+export const Graphic = styled.div.attrs({
   className: "d-none d-md-block col-4 position-absolute",
 })`
-  ${(props) => (props.indent === 1 ? "right" : "left")}: 0;
+  ${(props) => (props.indent === 0 ? "right" : "left")}: 0;
+`;
+
+export const Sprinkle = styled.img`
+  position: absolute;
+  ${(props) =>
+    props.indent === 0
+      ? `
+  width: 50%;
+  right: 0;
+  top: -15%;
+  `
+      : `
+  width: 100%;
+  transform: translate(-15px, -15px);
+  `}
+`;
+
+export const Image = styled(RoundImage)`
+  margin-bottom: 0;
+  z-index: ${OVERLAY_ZINDEX};
+  position: relative;
 `;
 
 export const MobileImageContainer = styled.div.attrs({
-  className: "d-md-none row",
+  className: "d-md-none row position-relative",
 })`
-  flex-direction: ${(props) => (props.indent === 1 ? "row-reverse" : "row")};
+  flex-direction: ${(props) => (props.indent === 0 ? "row-reverse" : "row")};
 `;
 
-export const MobileImage = styled(RoundImage).attrs({ className: "col-8" })``;
+export const MobileImage = styled(RoundImage).attrs({ className: "col-8" })`
+  z-index: ${OVERLAY_ZINDEX};
+  position: relative;
+`;
+
+export const MobileSprinkle = styled.img`
+  position: absolute;
+  ${(props) =>
+    props.indent === 0
+      ? `
+  width: 33%;
+  right: 0;
+  top: -15%;
+  `
+      : `
+  width: 66.67%;
+  transform: translate(0px, -15px);
+  `}
+`;
